f7c49b075a6ba5e5951ede9b2554041972ac0783
idem <victoridongesit@gmail.com>
2017-02-12 22:12:54 +0100

    Defined Skeleton for Prime Number Tests

---
+++ spec/GetPrimesSpec.js
@@ -0,0 +1,27 @@
+describe("Get Prime Numbers Test"){
+	describe("For edge cases", function(){
+		it('should return X for Y', function(){
+
+		});
+		it('should return X for Y', function(){
+
+		});
+		it('should return X for Y', function(){
+
+		}),
+	});
+	describe('For correct integer inputs', function(){
+		it('should return X for Y', function(){
+
+		});
+		it('should return X for Y', function(){
+
+		});
+		it('should return X for Y', function(){
+
+		});
+		it('should return X for Y', function(){
+
+		});
+	});
+};
