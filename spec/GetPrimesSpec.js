8a66b435f26d7d219a890375d0933c9a5cc20dbb
idem <victoridongesit@gmail.com>
2017-02-13 10:16:07 +0100

    Corrected Get Primes Algorithm to include upper n, added a test to that effect

--- spec/GetPrimesSpec.js
+++ spec/GetPrimesSpec.js
@@ -4,28 +4,31 @@ describe('Get Prime Numbers Test', function(){
 
 	describe("For edge cases", function(){
 
-		it('should return X for Y', function(){
+		it('should return "invalid input" for -2 ', function(){
 			expect(getPrimes(-2)).toEqual('invalid input');
 		});
-		it('should return X for Y', function(){
+		it('should return "invalid input" for "prime" for Y', function(){
 			expect(getPrimes('prime')).toEqual('invalid input');
 		});
-		it('should return X for Y', function(){
+		it('should return "invalid input" for 0', function(){
 			expect(getPrimes(0)).toEqual('invalid input');
 		});
-		it('should return X for Y', function(){
+		it('should return "invalid input" for 2.5', function(){
 			expect(getPrimes(2.5)).toEqual('invalid input');
 		});
 	});
 	describe('For correct integer inputs', function(){
-		it('should return X for Y', function(){
+		it('should return [2,3,5,7,11,13,17,19] for 20', function(){
 			expect(getPrimes(20)).toEqual([2,3,5,7,11,13,17,19]);
 		});
-		it('should return X for Y', function(){
+		it('should return [] for 1', function(){
 			expect(getPrimes(1)).toEqual([]);
 		});
-		it('should return X for Y', function(){
+		it('should return [2,3,5,7] for 8', function(){
 			expect(getPrimes(8)).toEqual([2,3,5,7]);
+		});
+		it('should return [2,3,5,7,11] for 11', function(){
+			expect(getPrimes(11)).toEqual([2,3,5,7,11]);
 		});
 	});
 });
--- src/GetPrimes.js
+++ src/GetPrimes.js
@@ -11,17 +11,17 @@ module.exports = {
 		const upperLimit = Math.sqrt(n);
 		const fullList = [];
 		const result = [];
-		for(var i = 0; i < n; i++){
+		for(var i = 0; i <= n; i++){
 			fullList.push(true);
 		}
 		for(var m = 2; m < upperLimit; m++){
 			if(fullList[m]){
-				for(j = m*m; j < n; j+=m){
+				for(j = m*m; j <= n; j+=m){
 					fullList[j] = false;
 				}
 			}
 		}
-		for(var k = 2; k < n; k++){
+		for(var k = 2; k <= n; k++){
 			if(fullList[k]){
 				result.push(k);
 			}
